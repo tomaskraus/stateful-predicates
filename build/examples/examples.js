b55c9661b59bd7b8e572e79e1f2ce16e6b80ea1f
Tomas Kraus <tomas.kraus.dev@gmail.com>
2022-11-21 00:43:16 +0100

    feat: add trueOneAfter function

--- build/examples/examples.js
+++ build/examples/examples.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const truesince_1 = require("../src/truesince");
+const src_1 = require("../src");
 const txt = `Begin of a file 
 Not so important stuff.
 
@@ -10,6 +10,16 @@ this is a footer section
 some text...`;
 const lines = txt.split('\n');
 // prettier-ignore
-const footerLines = lines.filter((0, truesince_1.trueSince)(s => /\[footer]/i.test(s)));
-console.log(footerLines);
+const footerLinesInclude = lines.filter((0, src_1.trueSince)(s => /\[footer]/i.test(s)));
+console.log('footer lines include:', footerLinesInclude);
+const footerLines = lines.filter((0, src_1.trueSince)((0, src_1.trueOneAfter)(s => /\[footer]/i.test(s))));
+console.log('footer lines:', footerLines);
+// ------------------------
+const isEven = (x) => x % 2 === 0;
+const result0 = [3, 2, 5, 7, 4, 1].map(isEven);
+console.log('result0:', result0);
+//=> result0: [ false, true, false, false, true, false ]
+const result1 = [3, 2, 5, 7, 4, 1].map((0, src_1.trueOneAfter)(isEven));
+console.log('result1:', result1);
+//=> result1: [ false, false, true, false, false, true ]
 //# sourceMappingURL=examples.js.map
--- build/src/index.js
+++ build/src/index.js
@@ -15,4 +15,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./truesince"), exports);
+__exportStar(require("./trueoneafter"), exports);
 //# sourceMappingURL=index.js.map
